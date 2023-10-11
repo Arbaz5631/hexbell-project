b278b38f19a36acee9e6c619db430314c0f7e239
iamarbazkhan <arbaz@leewayhertz.com>
2023-10-11 20:01:06 +0530

    ffewfew

--- src/HeaderComponet.js
+++ src/HeaderComponet.js
@@ -8,12 +8,12 @@ function HeaderComponet(props) {
       </div>
       <div className={styles.headerIcons}>
         <p className={styles.shopText}>Shop</p>
-        <img className={styles.headerLogo} src={"/images/hamburger.svg"} />
+        <img className={styles.headerLogo} src={"./hexbell-project/images/hamburger.svg"} />
         <div className={styles.cartIconDiv} onClick={()=>{props.setShowCart(true)}}>
-          <img className={styles.headerLogo} src={"/images/shopping.png"} />
+          <img className={styles.headerLogo} src={"./hexbell-project/images/shopping.png"} />
           <p className={styles.cartCount}>{props?.cartCount}</p>
         </div>
-        <img className={styles.headerLogo} src={"/images/hamburger.svg"} />
+        <img className={styles.headerLogo} src={"./hexbell-project/images/hamburger.svg"} />
       </div>
     </div>
   );
